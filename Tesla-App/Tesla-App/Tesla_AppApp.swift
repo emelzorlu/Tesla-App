9f746008e5f70380261b9eedf57ebca472810813
emel zorlu <emelzorlu@emel-MacBook-Air.local>
2024-03-21 15:30:43 +0300

    first commit

---
+++ Tesla-App/Tesla-App/Tesla_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tesla_AppApp.swift
+//  Tesla-App
+//
+//  Created by emel zorlu on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tesla_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
